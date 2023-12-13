5409671fc34545f089f041b176cb18154cce25b1
n3xtChanc3 <starsonthesky@protonmail.com>
2023-12-13 11:36:19 +0100

    updated setup.py file

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ setup(
     packages=find_packages(),
     install_requires=[],  # List your dependencies if any
     entry_points={},
-    author='decoder',
+    author='n3xtchanc3',
     author_email='starsonthesky@protonmail.com',
     description='A simple calculator package',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/your-username/simple-calculator',
+    url='https://github.com/n3xtChanc3/calculator',
     classifiers=[
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
